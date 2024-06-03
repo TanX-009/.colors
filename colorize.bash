a891b84cc9d00d10b903f850598445b2b7ac6c73
TanX-009 <tanmaymuley009@gmail.com>
2024-06-03 12:52:09 +0530

    updated colorize.bash to update swaync

--- colorize.bash
+++ colorize.bash
@@ -7,7 +7,8 @@ cd thex || exit
 python3 main.py -G -i "$wall"
 cd ..
 
-~/.dotfiles.config/.scripts/misc/launch-waybar.sh >.cache/waybar.log 2>&1
-~/.dotfiles.config/.scripts/misc/launch-swaybg.bash "$wall" >.cache/swaybg.log 2>&1
-~/.dotfiles.config/.scripts/misc/apply-ptm.bash >.cache/terminal.log 2>&1
-~/.dotfiles.config/.scripts/misc/apply-gradience.bash >.cache/gradience.log 2>&1
+~/.scripts/reload/launch-waybar.bash >.cache/waybar.log 2>&1
+~/.scripts/reload/launch-swaybg.bash "$wall" >.cache/swaybg.log 2>&1
+~/.scripts/reload/apply-ptm.bash >.cache/terminal.log 2>&1
+~/.scripts/reload/apply-swaync.bash >.cache/swaync.log 2>&1
+~/.scripts/reload/apply-gradience.bash >.cache/gradience.log 2>&1
